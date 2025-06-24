0a35451f8c2eb8a5800eecac51adf86724cd5c5c
Teva902 <ahmedricrdo510@gmail.com>
2025-06-25 02:57:21 +0300

    Init new app

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}>Hi Ahmed you are gamed</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // علشان النص يبقى في النص
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
